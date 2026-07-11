dd0c6028e9bb1fbd906ac0474d5c5f03f6328e45
Aajad Yadav <develop.contact.help@gmail.com>
2026-07-12 00:44:19 +0530

    feat: update package.json to include Prisma commands in build and postinstall scripts, and adjust axios baseURL in Api class for consistent local development

--- src/service/index.ts
+++ src/service/index.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 import { Script } from "./scripts";
 
 class Api {
-    axiosClient :AxiosInstance;
+    axiosClient: AxiosInstance;
     scripts: Script
-    constructor(){
+    constructor() {
         this.axiosClient = axios.create({
-            baseURL: import.meta.env.VITE_API_URL ?? "http://localhost:3000/api"
+            baseURL: "http://localhost:3000/api"
         })
         this.scripts = new Script(this.axiosClient)
     }
